55c18cf0e4a18119a1007eed62110bb95f3223ad
Lq7012 <167142282+Lq7012@users.noreply.github.com>
2024-04-16 10:34:33 +0300

    Update TypeOfAnimal.cs

--- Lq__/Lab-1/TypeOfAnimal.cs
+++ Lq__/Lab-1/TypeOfAnimal.cs
@@ -7,24 +7,56 @@ using Lab_1;
 
 namespace Lab_1
 {
-    internal class TypeOfAnimal : Animal
+    internal class Program
     {
-        // Наслідний клас
-        public string KindOfAnimal {  get; set; }
-
-        public void KindOfAnimals()
+        
+        public static void Main()
         {
-            Console.Write("Введіть вид тварини: ");
-            this.KindOfAnimal = Console.ReadLine();
-        }
-        public void Results()
-        {
-            Console.Clear();
-            Console.WriteLine("Вид тварини: " + KindOfAnimal +  "\nІм'я тварини: " + VarName + "\nВік тварини: " + VarAge + "\nДія тварини: " + VarAction);
-            Console.WriteLine("Дія... ");
-            Thread.Sleep(2000);
-            Console.WriteLine("Дія тварини " + '"' + VarAction + '"' + ": " + "завершена.");
-            Console.ReadLine();
+            // Виконувальний файл
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("1 - Собака, 2 - Тварина, 3 - Кіт, 4 - Своя тварина: ");
+            string animalChoice = Console.ReadLine();
+            if (animalChoice == "1") 
+            {
+                var dog = new CustomAnimal();
+                dog.Name();
+                dog.Age();
+                dog.PerformAction();
+                dog.ShowResults();
+            }
+            else if (animalChoice == "2") 
+            {
+                var animal = new CustomAnimal();
+                animal.Name();
+                animal.Age();
+                animal.PerformAction();
+                animal.ShowResults();
+            }
+            else if (animalChoice == "3") 
+            {
+                var cat = new CustomAnimal();
+                cat.Name();
+                cat.Age();
+                cat.PerformAction();
+                cat.ShowResults();
+            }
+            else if (animalChoice == "4") 
+            {
+                var customAnimal = new CustomAnimal();
+                customAnimal.KindOfAnimals();
+                customAnimal.Name();
+                customAnimal.Age();
+                customAnimal.PerformAction();
+                customAnimal.ShowResults();
+            }
+            else
+            {
+                Console.WriteLine("Число введено неправильно");
+            }
+            
+            var animal = new CustomAnimal();
+            var cat = new CustomAnimal();
+            var customAnimal = new CustomAnimal(); 
         }
     }
 }
