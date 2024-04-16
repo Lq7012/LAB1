f87c05388fc0ca5ab8716495f5e6b5cb5cc3a0ed
Lq7012 <167142282+Lq7012@users.noreply.github.com>
2024-04-16 10:36:28 +0300

    Update Animal.cs

--- Lq__/Lab-1/Animal.cs
+++ Lq__/Lab-1/Animal.cs
@@ -4,36 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+using System.Threading;
+
 namespace Lab_1
 {
-
     internal class Animal : IPatient
     {
         // Клас тварини
         public string VarName { get; set; }
         public string VarAge { get; set; }
         public string VarAction { get; set; }
+
         public void Name()
         {
             Console.Write("Введіть ім'я тварини: ");
-            this.VarName = Console.ReadLine();
+            VarName = Console.ReadLine();
         }
+
         public void Age()
         {
             Console.Write("Введіть вік тварини: ");
-            this.VarAge = Console.ReadLine();
+            VarAge = Console.ReadLine();
         }
+
         public void Action()
         {
-            Console.Write("Введіть дію тварни: ");
-            this.VarAction = Console.ReadLine();
+            Console.Write("Введіть дію тварини (їсть, спить, грається і т.д.): ");
+            VarAction = Console.ReadLine();
         }
+
         public void Results()
         {
             Console.Clear();
-            Console.WriteLine("Ім'я тварини: " + VarName + "\nВік тварини: " + VarAge + "\nдія тварни: " + VarAction);
+            Console.WriteLine("Ім'я тварини: " + VarName + "\nВік тварини: " + VarAge + "\nДія тварини: " + VarAction);
             Thread.Sleep(2000);
-            Console.WriteLine("дія тварини " + '"' + VarAction + '"' + ": " + "завершена.");
+            Console.WriteLine("Дія тварини " + '"' + VarAction + '"' + ": " + "завершена.");
             Console.ReadLine();
         }
     }
